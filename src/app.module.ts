67df58d6c09118a44baa53a50541a6a816421b62
mdriaz60000 <mohammadriaz60000@gmail.com>
2025-08-07 16:52:43 +0600

    users added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { MongooseModule } from '@nestjs/mongoose';
       isGlobal: true,
     }),
     MongooseModule.forRoot(process.env.MONGODB_URI as string),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/users/users.service.ts
@@ -0,0 +1,44 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from './schemas/user.schema';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  create(createUserDto: CreateUserDto) {
+    const newUser = new this.userModel(createUserDto);
+    return newUser.save();
+  }
+
+  findAll() {
+    return this.userModel.find();
+  }
+
+  async findOne(id: string) {
+    const user = await this.userModel.findById(id);
+    if (!user) throw new NotFoundException('User not found');
+    return user;
+  }
+
+  async update(id: string, updateUserDto: UpdateUserDto) {
+    const updated = await this.userModel.findByIdAndUpdate(
+      id,
+      updateUserDto,
+      {
+        new: true,
+      }
+    );
+    if (!updated) throw new NotFoundException('User not found');
+    return updated;
+  }
+
+  async remove(id: string) {
+    const deleted = await this.userModel.findByIdAndDelete(id);
+    if (!deleted) throw new NotFoundException('User not found');
+    return deleted;
+  }
+}
