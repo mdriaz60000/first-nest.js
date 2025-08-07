882a8d9a1b760950cbe9c22f001c97913ef8b64d
mdriaz60000 <mohammadriaz60000@gmail.com>
2025-08-07 13:39:39 +0600

    create nest.js

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI as string),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
